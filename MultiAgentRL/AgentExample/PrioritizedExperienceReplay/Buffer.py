2c2a0f56051cfe1a9afe429f5a72b55a8ef06eac
raharrasy <rahmanarrasy@gmail.com>
2018-03-21 11:03:15 +0800

    Fixing bugs in weight based experience replay.

--- MultiAgentRL/AgentExample/PrioritizedExperienceReplay/Buffer.py
+++ MultiAgentRL/AgentExample/PrioritizedExperienceReplay/Buffer.py
@@ -1,14 +1,15 @@
-def __init__(self,maxSize):
-	self.storage = [None] * maxSize
-	self.pointer = 0
-	self.maxSize = maxSize
+class Buffer(object):
+    def __init__(self,maxSize):
+        self.storage = [None] * maxSize
+        self.pointer = 0
+        self.maxSize = maxSize
 
-def insert(experience):
-	self.storage[self.pointer] = experience
-	self.pointer = (self.pointer+1)%maxSize
+    def insert(self,experience):
+        self.storage[self.pointer] = experience
+        self.pointer = (self.pointer+1)%self.maxSize
 
-def getItem(index):
-	return self.storage[index]
+    def getItem(self,index):
+        return self.storage[index]
 
-def getPointer():
-	return self.pointer
+    def getPointer(self):
+        return self.pointer
--- MultiAgentRL/AgentExample/PrioritizedExperienceReplay/SumTree.py
+++ MultiAgentRL/AgentExample/PrioritizedExperienceReplay/SumTree.py
@@ -1,69 +1,74 @@
-def __init__(self, maxSize):
-	self.maxSize = maxSize
-	self.root = initialize(maxSize)
-	self.maxDepth = 0
-	multiplier = 1
-	while multiplier <= self.maxSize:
-		self.maxDepth+=1
-		multiplier *= 2
-	initialize(maxSize)
+import math
 
-def getAllSum(self):
+class SumTree(object):
+    def __init__(self, maxSize):
+        self.maxSize = maxSize
+        self.root = initialize(maxSize)
+        self.maxDepth = 0
+        multiplier = 1
+        while multiplier <= self.maxSize:
+            self.maxDepth+=1
+            multiplier *= 2
+
+    def getAllSum(self):
         return self.root.getSum()
 
+    def search(self,priority):
+        searchNode = self.root
+        steps = []
+        while searchNode.left or searchNode.right:
+            if priority < searchNode.left.getSum():
+                searchNode = searchNode.left
+                steps.append(0)
+            else:
+                searchNode = searchNode.right
+                steps.append(1)
+        length = len(steps)
+        multiplier = [2**(length-i-1) for i in range(length)]
+        return sum([i*j for (i, j) in zip(steps, multiplier)])
+
+    def insert(self,priorityDiff, index):
+        initial = "{0:b}".format(index)
+        adder = "0" * (self.maxDepth-len(initial))
+        searchString = adder+initial
+        searchNode = self.root
+        for a in range(len(searchString)):
+            charS = searchString[a]
+            searchNode.sum += priorityDiff
+            if charS == '0':
+                searchNode = searchNode.left
+            else:
+                searchNode = searchNode.right
+        searchNode.sum += priorityDiff
+
+class Node(object):
+    def __init__(self, sum=0,right = None, left = None):
+        self.sum = sum
+        self.right = right
+        self.left = left
+
+    def getSum(self):
+        return self.sum
+
+    
 def initialize(maxSize):
-	lst = [Node() for i in range(maxSize)]
-	while len(lst) > 1:
-		buffer = []
-		for a in range((len(lst)/2)-1):
-			aggr = Node(lst[2*a].getSum()+lst[2*a+1].getSum(),lst[2*a+1],lst[2*a])
-			buffer.append(aggr)
-		if len(lst) % 2 == 0:
-			aggr = Node(lst[2*a].getSum()+lst[2*a+1].getSum(),lst[2*a+1],lst[2*a])
-                        buffer.append(aggr)
-		else:
-			aggr = Node(sum=lst[2*a].getSum()+lst[2*a+1].getSum(),left=lst[2*a])
-                        buffer.append(aggr)
+    lst = []
+    for a in range(2**math.ceil(math.log2(maxSize))):
+        lst.append(Node())
+    while len(lst) > 1:
+        buffer = []
+        for a in range((len(lst)//2)-1):
+            aggr = Node(lst[2*a].getSum()+lst[2*a+1].getSum(),lst[2*a+1],lst[2*a])
+            buffer.append(aggr)
+            
+        a = (len(lst)//2) - 1
+        if len(lst) % 2 == 0:
+            aggr = Node(lst[2*a].getSum()+lst[2*a+1].getSum(),lst[2*a+1],lst[2*a])
+            buffer.append(aggr)
+        else:
+            aggr = Node(sum=lst[2*a].getSum(),left=lst[2*a])
+            buffer.append(aggr)
 
-		lst = buffer
-	
-
-def search(self,priority):
-	searchNode = self.root
-	steps = []
-	while searchNode.left or searchNode.right:
-		if priority < searchnode.getSum():
-			searchNode = searchNode.left
-			steps = steps.append(0)
-		else:
-			searchNode = searchNode.right
-			steps = steps.append(1)
-	length = len(steps)
-	multiplier = [2**(length-i-1) for i in range(length)]
-	return sum([i*j for (i, j) in zip(steps, multiplier)])
-
-def insert(self,priorityDiff, index):
-	initial = "{0:b}".format(index)
-	adder = "0" * (self.maxDepth-len(initial))
-	searchString = adder+initial
-
-	searchNode = self.root
-	for a in range(len(searchString)):
-		charS = searchString[a]
-		searchNode.sum += priorityDiff
-		if charS == 0:
-			searchNode = searchNode.left
-		else:
-			searchNode = searchNode.right
-	searchnode.sum += priorityDiff
-
-class Node:
-	def __init__(self, sum=0,right = None, left = None):
-		self.sum = sum
-		self.right = right
-		self.left = left
-
-	def getSum():
-		return self.sum
-
+        lst = buffer
+    return lst[0]
 
